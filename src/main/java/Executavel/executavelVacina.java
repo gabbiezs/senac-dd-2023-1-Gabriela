8d8706dcb91854df47afe2298e280d9c2d7442d7
Gabbie <Gabbie@DESKTOP-NE5691D>
2023-03-27 19:26:46 -0300

    Atualização do banco, do executavel e correção dos atributos nos métodos para alinhar com o banco.

--- src/main/java/Executavel/executavelVacina.java
+++ src/main/java/Executavel/executavelVacina.java
@@ -1,21 +1,52 @@
 package Executavel;
 
+import java.time.LocalDate;
+import model.dao.vacina.PessoaDAO;
+import model.dao.vacina.VacinaDAO;
+import model.vo.vacina.EstagioPesquisa;
 import model.vo.vacina.Pessoa;
+import model.vo.vacina.TipoPessoa;
 import model.vo.vacina.Vacina;
 
 public class executavelVacina {
 
 	public static void main(String[] args) {
-		// 1- Pesquisadores, 2- Voluntarios, 3- Geral
-		// executavel não finalizado, rascunho
 
-	//	Pessoa p1 = new Pessoa(null,"Luciano Lazaro", "20/04/1987", "M", "123.456.789-11", 1);
-	//	Pessoa p2 = new Pessoa(null,"Maria Madalena", "13/07/1983", "F", "134.435.768-13", 2);
-	//	Pessoa p3 = new Pessoa(null,"Carlos Claudio", "09/10/1991", "M", "123.456.789-11", 3);
+		// executavel nï¿½o finalizado, rascunho
+
+		Pessoa p1 = new Pessoa(null,"Luciano Lazaro", "20/04/1987", "M", "123.456.789-11", TipoPessoa.PESQUISADOR);
+		Pessoa p2 = new Pessoa(null,"Maria Madalena", "13/07/1983", "F", "134.435.768-13", TipoPessoa.VOLUNTARIO);
+		Pessoa p3 = new Pessoa(null,"Carlos Claudio", "09/10/1991", "M", "123.456.749-14", TipoPessoa.GERAL);
 		
-	//	Vacina v1 = new Vacina(null,"Vacina Azul", "Alemanha", 1, "23/07/2022", p1);
-	//	Vacina v2 = new Vacina(null,"Vacina Amarela", "Peru", 2, "05/02/2021", p1);
-	//	Vacina v3 = new Vacina(null,"Vacina Vermelha", "China", 3, "11/01/2023", p1);
+		PessoaDAO cadastrarPessoa = new PessoaDAO();
+		cadastrarPessoa.cadastrarPessoa(p1);
+		
+//		Pessoa pessoaConsultada = cadastrarPessoa.consultarPorId(1);
+//		pessoaConsultada.setCpf("456.123.987-15");
+//		boolean atualizou = cadastrarPessoa.atualizar(pessoaConsultada);		
+//		if (atualizou) {
+//			System.out.println("Pessoa atualizada com sucesso.");
+//		}
+
+//		if (cadastrarPessoa.excluir(2)) {
+//			System.out.println("Pessoa foi excluida!");
+//		}
+		
+		// Consultar Todos
+//		for (Pessoa pessoa : cadastrarPessoa.consultarTodos()) {
+//			System.out.println(pessoa.getNome());
+//		}
+		
+		// Consultar pessoa por id
+//		System.out.println(cadastrarPessoa.consultarPorId(1).getNome());
+		
+
+		Vacina v1 = new Vacina(null,"Vacina da Gripe", "Alemanha", EstagioPesquisa.INICIAL, LocalDate.now(), cadastrarPessoa.consultarPorId(1));
+		Vacina v2 = new Vacina(null,"Vacina da Dengue", "Peru", EstagioPesquisa.TESTES, LocalDate.now(), cadastrarPessoa.consultarPorId(1));
+		Vacina v3 = new Vacina(null,"Vacina da Febre Amarela", "China", EstagioPesquisa.MASSA, LocalDate.now(), cadastrarPessoa.consultarPorId(1));
+		VacinaDAO cadastrarVacina = new VacinaDAO();
+		cadastrarVacina.cadastrarVacina(v1);
+		
 	}
 
 }
